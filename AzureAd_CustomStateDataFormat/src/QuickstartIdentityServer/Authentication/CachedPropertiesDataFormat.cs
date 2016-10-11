2e49cd2d5ffedfc5fa1f7adbbfd0818b6186ac2f
Michael Clark <mclark1129@gmail.com>
2016-10-10 22:10:08 -0400

    Reformatted CachedPropertiesDataFormat and added some comments

--- AzureAd_CustomStateDataFormat/src/QuickstartIdentityServer/Authentication/CachedPropertiesDataFormat.cs
+++ AzureAd_CustomStateDataFormat/src/QuickstartIdentityServer/Authentication/CachedPropertiesDataFormat.cs
@@ -11,10 +11,13 @@ using Microsoft.Extensions.Caching.Distributed;
 namespace QuickstartIdentityServer.Authentication
 {
     /// <summary>
-    /// 
+    /// Custom secure data format intended for use with OpenIdConnectMiddleware.
+    /// It is intended to reduce the size of the state parameter generated
+    /// by the default PropertiesDataFormat, which results in query strings
+    /// that are too long for Azure AD to handle.
     /// </summary>
-    public class CachedPropertiesDataFormat 
-        : ISecureDataFormat<AuthenticationProperties>        
+    public class CachedPropertiesDataFormat
+        : ISecureDataFormat<AuthenticationProperties>
     {
 
         public const string CacheKeyPrefix = "CachedPropertiesData-";
@@ -25,50 +28,53 @@ namespace QuickstartIdentityServer.Authentication
 
         public CachedPropertiesDataFormat(
             IDistributedCache cache,
-            IDataProtector dataProtector) 
-            : this(cache, dataProtector, new PropertiesSerializer()) {
+            IDataProtector dataProtector)
+            : this(cache, dataProtector, new PropertiesSerializer())
+        {
 
         }
 
         public CachedPropertiesDataFormat(
             IDistributedCache cache,
             IDataProtector dataProtector,
-            IDataSerializer<AuthenticationProperties> serializer) {
-
+            IDataSerializer<AuthenticationProperties> serializer)
+        {
             _dataProtector = dataProtector;
             _cache = cache;
             _serializer = serializer;
-
         }
 
-        public string Protect(AuthenticationProperties data) {
+        public string Protect(AuthenticationProperties data)
+        {
             return Protect(data, null);
         }
 
-        public string Protect(AuthenticationProperties data, string purpose) {
-
+        public string Protect(AuthenticationProperties data, string purpose)
+        {
             var key = Guid.NewGuid().ToString();
             var cacheKey = $"{CacheKeyPrefix}{key}";
             var serialized = _serializer.Serialize(data);
 
+            // Rather than encrypt the full AuthenticationProperties
+            // cache the data and encrypt the key that points to the data
             _cache.Set(cacheKey, serialized);
 
             return _dataProtector.Protect(key);
-
         }
 
-        public AuthenticationProperties Unprotect(string protectedText) {
+        public AuthenticationProperties Unprotect(string protectedText)
+        {
             return Unprotect(protectedText, null);
         }
 
-        public AuthenticationProperties Unprotect(string protectedText, string purpose) {
-
+        public AuthenticationProperties Unprotect(string protectedText, string purpose)
+        {
+            // Decrypt the 
             var key = _dataProtector.Unprotect(protectedText);
             var cacheKey = $"{CacheKeyPrefix}{key}";
             var serialized = _cache.Get(cacheKey);
 
             return _serializer.Deserialize(serialized);
-
         }
 
     }
